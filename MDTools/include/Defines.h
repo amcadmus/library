83e5c4b2ffabe5833d8f26c50a5f9e92aeabb88b
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2011-12-27 21:03:59 +0000

    add MDTools

---
+++ MDTools/include/Defines.h
@@ -0,0 +1,16 @@
+#ifndef __Defines_h_wanghan__
+#define __Defines_h_wanghan__
+
+typedef float ValueType ;
+
+struct IntVectorType
+{
+  int x, y, z;
+};
+
+struct VectorType
+{
+  ValueType x, y, z;
+};
+
+#endif
